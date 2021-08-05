894d35d739b52458002a7f2fb81fee1091b345ed
Ivan Velev <vanecat@gmail.com>
2021-08-05 01:36:11 -0700

    added calendar full-view of year's months

---
+++ tests/calendar-year.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Test App</title>
+    <script src="../init.js"></script>
+    <script src="../model.js"></script>
+    <script src="../data.js"></script>
+    <script src="../vue.js"></script>
+</head>
+<body>
+<?php require_once '../components/calendar-month.html'; ?>
+<div id="test-app">
+    <div v-for="msg in msgs">{{msg}}</div>
+    <calendar-month v-for="month in year.children" :key="month.id" :month="month"></calendar-month>
+</div>
+<script>
+    Pywb.vue.app = {
+        el: '#test-app',
+        data: {
+            msgs: [],
+            year: {}
+        },
+        mounted() {
+            this.addMsg('test app is loaded');
+            this.loadData();
+        },
+        methods: {
+            loadData() {
+                fetch('/sample-data/uk-gov.json').then(r => r.json()).then(data => this.onLoadData(data));
+            },
+            onLoadData(data) {
+                try {
+                    const {timeline, linear} = Pywb.makeData(data);
+                    this.year = timeline.getChildById(2020);
+                } catch(e) {
+                    this.addMsg(e.message);
+                }
+            },
+            addMsg(msg) {
+                this.msgs.push(msg);
+            }
+        }
+    };
+    Pywb.init();
+</script>
+</body>
+</html>
