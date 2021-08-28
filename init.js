11321d225c06ccbf20e289b5ccfff1ffc9532139
Ivan Velev <vanecat@gmail.com>
2021-08-27 19:54:05 -0700

    convert vue component templates into ordinary html: prep for production packing/minifying via script

--- init.js
+++ init.js
@@ -1,10 +1,19 @@
 const Pywb = {
     data: {},
-    vue: {app:{}, components:{} },
+    vue: {app:{}, components:{}},
     init: () => {
+        // components templates
+        document.querySelectorAll('[data-template]').forEach((item) => {
+            Pywb.vue.components[item.dataset.template].template = item.innerHTML;
+            item.parentElement.removeChild(item);
+        });
+
+        // components
         Object.keys(Pywb.vue.components).forEach(function(componentId) {
             Vue.component(componentId, Pywb.vue.components[componentId]);
         });
+
+        // main app
         Pywb.vue.app = new Vue(Pywb.vue.app);
     }
 };
