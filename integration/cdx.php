7426f6a52c0fe7045d5ad877922d9db9f7b61ab3
Ivan Velev <vanecat@gmail.com>
2021-08-31 05:34:19 -0700

    allow test data loading for dynamic data

--- integration/cdx.php
+++ integration/cdx.php
@@ -1,6 +1,7 @@
 <?php
 
-$data = json_decode(file_get_contents('../sample-data/bbc-com.json'));
+$url = str_replace('.', '-', preg_replace('@^https?://(.*)$@', '$1', $_GET['url']));
+$data = json_decode(file_get_contents('../sample-data/'.$url.'.json'));
 foreach($data as $item) {
     echo json_encode($item);
     echo "\n";
